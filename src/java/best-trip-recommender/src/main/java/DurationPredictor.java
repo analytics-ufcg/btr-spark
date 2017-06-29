5ea4a89b68627ac08cb900f6e78232a19a71d61e
odwl66 <orion.lima@ccc.ufcg.edu.br>
2017-06-29 11:52:31 -0300

    Trying to add categorical features, but not succeded

--- src/java/best-trip-recommender/src/main/java/DurationPredictor.java
+++ src/java/best-trip-recommender/src/main/java/DurationPredictor.java
@@ -5,7 +5,10 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.ml.Pipeline;
+import org.apache.spark.ml.PipelineStage;
 import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.feature.VectorIndexer;
 import org.apache.spark.ml.regression.LinearRegression;
 import org.apache.spark.ml.regression.LinearRegressionModel;
 import org.apache.spark.ml.regression.LinearRegressionTrainingSummary;
@@ -23,37 +26,62 @@ public class DurationPredictor {
 		// Load training data
 		DataFrame training = sqlContext.read()
 				.format("com.databricks.spark.csv")
-				.option("header","true")
+				.option("header", "true")
 				.option("inferSchema", "true")
 				.load(args[0]);
-		
+
 		training.show(1);
 
 		training.printSchema();
 		
-		VectorAssembler assembler = new VectorAssembler()
-				.setInputCols(new String[]{"departure", "arrival"})
-				.setOutputCol("features");
+		// Automatically identify categorical features, and index them.
+		// Set maxCategories so features with > 4 distinct values are treated as
+		// continuous.
+		String[] categoricalFeatures = {"route", "week_day"};
+		PipelineStage[] featuresIndexers = new PipelineStage[categoricalFeatures.length];
 		
-		DataFrame trainingDF = assembler.transform(training);
+		for (int i = 0; i < categoricalFeatures.length; i++) {
+			featuresIndexers[i] = new VectorIndexer()
+					.setInputCol(categoricalFeatures[i])
+					.setOutputCol(categoricalFeatures[i] + "_index")
+					// Number of routes
+					.setMaxCategories(240);
+		}
+		
+		Pipeline pipeline = new Pipeline()
+				  .setStages(featuresIndexers);
+		
+		DataFrame trainingDF = pipeline
+				.fit(training)
+				.transform(training);
 		
 		trainingDF.show(1);
 
-		trainingDF.printSchema();		
+		trainingDF.printSchema();
+
+		VectorAssembler assembler = new VectorAssembler()
+				.setInputCols(new String[] {"departure", "arrival", "route", "week_day",
+						"difference_previous_schedule", "difference_next_schedule"})
+				.setOutputCol("features");
+
+		trainingDF = assembler.transform(training);
+
+		trainingDF.show(1);
+
+		trainingDF.printSchema();
 
 		LinearRegression lr = new LinearRegression()
-		  .setMaxIter(10)
-		  .setRegParam(0.3)
-		  .setElasticNetParam(1.0)
-		  .setLabelCol("duration")
-		  .setFeaturesCol("features");
-
+				.setMaxIter(10)
+				.setRegParam(0.3)
+				.setElasticNetParam(1.0)
+				.setLabelCol("duration")
+				.setFeaturesCol("features");
+		
 		// Fit the model
 		LinearRegressionModel lrModel = lr.fit(trainingDF);
 
 		// Print the coefficients and intercept for linear regression
-		System.out.println("Coefficients: "
-		  + lrModel.coefficients() + " Intercept: " + lrModel.intercept());
+		System.out.println("Coefficients: " + lrModel.coefficients() + " Intercept: " + lrModel.intercept());
 
 		// Summarize the model over the training set and print out some metrics
 		LinearRegressionTrainingSummary trainingSummary = lrModel.summary();
