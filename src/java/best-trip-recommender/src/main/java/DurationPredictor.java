45c035aecb055843d44df8b1ae7aa03b332953ff
odwl66 <orion.lima@ccc.ufcg.edu.br>
2017-06-29 10:32:37 -0300

    Training linear regression model with java and numerical features

--- src/java/best-trip-recommender/src/main/java/DurationPredictor.java
+++ src/java/best-trip-recommender/src/main/java/DurationPredictor.java
@@ -1,62 +1,69 @@
 
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.apache.spark.SparkConf;
-import org.apache.spark.ml.feature.VectorIndexer;
-import org.apache.spark.ml.feature.VectorIndexerModel;
+import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.ml.regression.LinearRegression;
 import org.apache.spark.ml.regression.LinearRegressionModel;
 import org.apache.spark.ml.regression.LinearRegressionTrainingSummary;
 import org.apache.spark.mllib.linalg.Vectors;
-import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.DataFrame;
-import org.apache.spark.sql.DataFrameReader;
 import org.apache.spark.sql.SQLContext;
 
 public class DurationPredictor {
-	
-	
-	public static void main(String[] args) throws URISyntaxException, IOException {
-	    SparkConf sconf = new SparkConf().setAppName("Duration Predictor").setMaster("local[*]");
-	    JavaSparkContext jsc = new JavaSparkContext(sconf);
-	    SQLContext sqlContext = new SQLContext(jsc);
-	    
-	    // Load training data
-	    DataFrame training = sqlContext.read().format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load(args[0]);
-	    
-	    training.show(1);
-	   
-	    LinearRegression lr = new LinearRegression()
-	      .setMaxIter(10)
-	      .setRegParam(0.3)
-	      .setElasticNetParam(1.0);
-	    
-	    // Fit the model
-//	    LinearRegressionModel lrModel = lr.fit(training);
-//
-//	    // Print the coefficients and intercept for linear regression
-//	    System.out.println("Coefficients: "
-//	      + lrModel.coefficients() + " Intercept: " + lrModel.intercept());
-//
-//	    // Summarize the model over the training set and print out some metrics
-//	    LinearRegressionTrainingSummary trainingSummary = lrModel.summary();
-//	    System.out.println("numIterations: " + trainingSummary.totalIterations());
-//	    System.out.println("objectiveHistory: " + Vectors.dense(trainingSummary.objectiveHistory()));
-//	    trainingSummary.residuals().show();
-//	    System.out.println("RMSE: " + trainingSummary.rootMeanSquaredError());
-//	    System.out.println("r2: " + trainingSummary.r2());
-	    // $example off$
 
-	    jsc.stop();
-	
+	public static void main(String[] args) throws URISyntaxException, IOException {
+		SparkConf sconf = new SparkConf().setAppName("Duration Predictor").setMaster("local[*]");
+		JavaSparkContext jsc = new JavaSparkContext(sconf);
+		SQLContext sqlContext = new SQLContext(jsc);
+
+		// Load training data
+		DataFrame training = sqlContext.read()
+				.format("com.databricks.spark.csv")
+				.option("header","true")
+				.option("inferSchema", "true")
+				.load(args[0]);
+		
+		training.show(1);
+
+		training.printSchema();
+		
+		VectorAssembler assembler = new VectorAssembler()
+				.setInputCols(new String[]{"departure", "arrival"})
+				.setOutputCol("features");
+		
+		DataFrame trainingDF = assembler.transform(training);
+		
+		trainingDF.show(1);
+
+		trainingDF.printSchema();		
+
+		LinearRegression lr = new LinearRegression()
+		  .setMaxIter(10)
+		  .setRegParam(0.3)
+		  .setElasticNetParam(1.0)
+		  .setLabelCol("duration")
+		  .setFeaturesCol("features");
+
+		// Fit the model
+		LinearRegressionModel lrModel = lr.fit(trainingDF);
+
+		// Print the coefficients and intercept for linear regression
+		System.out.println("Coefficients: "
+		  + lrModel.coefficients() + " Intercept: " + lrModel.intercept());
+
+		// Summarize the model over the training set and print out some metrics
+		LinearRegressionTrainingSummary trainingSummary = lrModel.summary();
+		System.out.println("numIterations: " + trainingSummary.totalIterations());
+		System.out.println("objectiveHistory: " + Vectors.dense(trainingSummary.objectiveHistory()));
+		trainingSummary.residuals().show();
+		System.out.println("RMSE: " + trainingSummary.rootMeanSquaredError());
+		System.out.println("r2: " + trainingSummary.r2());
+
+		jsc.stop();
+
 	}
 }
