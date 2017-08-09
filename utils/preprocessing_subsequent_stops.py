4d228cc64fee2a221040a86da7e09a270c7b3bab
odwl66 <orion.lima@ccc.ufcg.edu.br>
2017-08-09 10:44:59 -0300

    Pre processing almost finished

--- utils/preprocessing_subsequent_stops.py
+++ utils/preprocessing_subsequent_stops.py
@@ -9,9 +9,10 @@ import pyspark
 from pyspark import SparkContext
 from pyspark.sql.functions import lit, lead, udf, unix_timestamp, hour, when, weekofyear, date_format, dayofmonth, month
 from pyspark.sql.window import Window
-from pyspark.sql.types import DoubleType
+from pyspark.sql.types import DoubleType, StringType
 
 from math import radians, cos, sin, asin, sqrt
+from datetime import datetime
 
 def read_file(filepath, sqlContext):
     data_frame = sqlContext.read.format("com.databricks.spark.csv") \
@@ -97,56 +98,60 @@ def haversine(lon1, lat1, lon2, lat2):
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
     return c * r
 
+def weekday(date, fmt = "%Y-%m-%d"):
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    wd = datetime.strptime(date, fmt).weekday()
+    return days[wd]
+
 def extract_features(df):
     print "Inicial features"
     print ["TRIP_NUM_ORIG", "ROUTE", "SHAPE_ID", "SHAPE_SEQ", "LAT_SHAPE_ORIG", "LON_SHAPE_ORIG", "GPS_POINT_ID",
-           "BUS_CODE", "TIMESTAMP_ORIG", "LAT_GPS", "LON_GPS", "DISTANCE_ORIG", "THRESHOLD_PROBLEM", "TRIP_PROBLEM",
-           "STOP_ID_ORIG", "DATE", "STOP_ID_DEST", "TIMESTAMP_DEST", "TRIP_NUM_DEST", "LAT_SHAPE_DEST",
-           "LON_SHAPE_DEST", "DISTANCE_DEST"]
+           "BUS_CODE", "TIMESTAMP_ORIG", "LAT_GPS", "LON_GPS", "DISTANCE_GPS_SHAPE", "THRESHOLD_PROBLEM",
+           "TRIP_PROBLEM", "STOP_ID_ORIG", "DATE", "STOP_ID_DEST", "TIMESTAMP_DEST", "TRIP_NUM_DEST", "LAT_SHAPE_DEST",
+           "LON_SHAPE_DEST"]
 
-    # TODO variable to predict
-    # TIMESTAMP_ORIG + TIMESTAMP_DEST      => Extract duration in seconds
+    # Extract duration in seconds
     time_fmt = "HH:mm:ss"
     time_difference = unix_timestamp("TIMESTAMP_DEST", time_fmt) - unix_timestamp("TIMESTAMP_ORIG", time_fmt)
     df = df.withColumn("DURATION", time_difference)
 
-    # TODO feature extraction
-    # TIMESTAMP_ORIG                       => Extract hour
+    # Extract hour
     df = df.withColumn("HOUR_ORIG", hour("TIMESTAMP_ORIG"))
-    # TIMESTAMP_DEST                       => Extract hour
+    # Extract hour
     df = df.withColumn("HOUR_DEST", hour("TIMESTAMP_DEST"))
-    df.printSchema()
-    # hour                                 => Extract is rush hour
+    # Extract is rush hour
     rush_hours_orig = when((df.HOUR_ORIG == 6) | (df.HOUR_ORIG == 7) | (df.HOUR_ORIG == 11) | (df.HOUR_ORIG == 12) | (df.HOUR_ORIG == 17) | (df.HOUR_ORIG == 18), 1).otherwise(0)
     df = df.withColumn("IS_RUSH_ORIG", rush_hours_orig)
-    # hour                                 => Extract is rush hour
+    # Extract is rush hour
     rush_hours_dest = when((df.HOUR_DEST == 6) | (df.HOUR_DEST == 7) | (df.HOUR_DEST == 11) | (df.HOUR_DEST == 12) | (df.HOUR_DEST == 17) | (df.HOUR_DEST == 18), 1).otherwise(0)
     df = df.withColumn("IS_RUSH_DEST", rush_hours_dest)
-    # hour                                 => Extract period of day
+    # Extract period of day
     period_orig = when(df.HOUR_ORIG < 12, "morning").otherwise(when((df.HOUR_ORIG >= 12) & (df.HOUR_ORIG < 18), "afternoon").otherwise("night"))
     df = df.withColumn("PERIOD_ORIG", period_orig)
-    # hour                                 => Extract period of day
-    period_orig = when(df.HOUR_DEST < 12, "morning").otherwise(when((df.HOUR_DEST >= 12) & (df.HOUR_DEST < 18), "afternoon").otherwise("night"))
-    df = df.withColumn("PERIOD_DEST", period_orig)
-    # DATE                                 => Extract week day
-    df = df.withColumn("WEEK_DAY", date_format("DATE", "E"))
-    # DATE                                 => Extract week number
+    # Extract period of day
+    period_dest = when(df.HOUR_DEST < 12, "morning").otherwise(when((df.HOUR_DEST >= 12) & (df.HOUR_DEST < 18), "afternoon").otherwise("night"))
+    df = df.withColumn("PERIOD_DEST", period_dest)
+    # Extract week day
+    udf_weekday = udf(weekday, StringType())
+    df = df.withColumn("WEEK_DAY", udf_weekday("DATE"))
+    # Extract week number
     df = df.withColumn("WEEK_OF_YEAR", weekofyear("DATE"))
-    # DATE                                 => Extract day of month
+    # Extract day of month
     df = df.withColumn("DAY_OF_MONTH", dayofmonth("DATE"))
-    # DATE                                 => Extract month
+    # Extract month
     df = df.withColumn("MONTH", month("DATE"))
-    # month                                => Extract is holidays
-    # week day                             => Extract is weekend
-    # week day                             => Extract is TUE, WED, THU
-    # DISTANCE_ORIG + DISTANCE_DEST        => Extract total distance
+    # Extract is holidays
+    is_holiday = when((df.MONTH == 1) | (df.MONTH == 6) | (df.MONTH == 7) | (df.MONTH == 12), 1).otherwise(0)
+    df = df.withColumn("IS_HOLIDAY", is_holiday)
+    # Extract is weekend
+    is_weekend = when((df.WEEK_DAY == "Sat") | (df.WEEK_DAY == "Sun"), 1).otherwise(0)
+    df = df.withColumn("IS_WEEKEND", is_weekend)
+    # Extract is TUE, WED, THU
+    is_regular_day = when((df.WEEK_DAY == "Tue") | (df.WEEK_DAY == "Wed") | (df.WEEK_DAY == "Thu"), 1).otherwise(0)
+    df = df.withColumn("IS_REGULAR_DAY", is_regular_day)
+    # Extract total distance
     haversine_udf = udf(haversine, DoubleType())
     df = df.withColumn("TOTAL_DISTANCE", haversine_udf("LON_SHAPE_ORIG", "LAT_SHAPE_ORIG", "LON_SHAPE_DEST", "LAT_SHAPE_DEST"))
-    # duration in seconds + total distance => Extract speed
-    # LAT_SHAPE_ORIG + LON_SHAPE_ORIG      => Extract region of city
-    # LAT_SHAPE_DEST + LON_SHAPE_DEST      => Extract region of city
-
-    df.filter(df.HOUR_ORIG == 7).show(10)
 
     return df
 
@@ -176,8 +181,7 @@ if __name__ == "__main__":
             ("TIMESTAMP", "TIMESTAMP_DEST"),
             ("TRIP_NUM", "TRIP_NUM_DEST"),
             ("LAT_SHAPE", "LAT_SHAPE_DEST"),
-            ("LON_SHAPE", "LON_SHAPE_DEST"),
-            ("DISTANCE", "DISTANCE_DEST")
+            ("LON_SHAPE", "LON_SHAPE_DEST")
         ],
         w
     )
@@ -190,7 +194,7 @@ if __name__ == "__main__":
             ("TRIP_NUM", "TRIP_NUM_ORIG"),
             ("LAT_SHAPE", "LAT_SHAPE_ORIG"),
             ("LON_SHAPE", "LON_SHAPE_ORIG"),
-            ("DISTANCE", "DISTANCE_ORIG")
+            ("DISTANCE", "DISTANCE_GPS_SHAPE")
         ]
     )
 
