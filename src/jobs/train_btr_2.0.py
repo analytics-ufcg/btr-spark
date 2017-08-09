55267354b7dc09147c3fd14f360eabe4c28a984d
odwl66 <orion.lima@ccc.ufcg.edu.br>
2017-08-09 17:18:40 -0300

    Predicting trip duration

--- src/jobs/train_btr_2.0.py
+++ src/jobs/train_btr_2.0.py
@@ -13,11 +13,15 @@ import os
 
 
 def read_data(sqlContext, filepath):
-    return sqlContext.read.format("com.databricks.spark.csv")\
+    df = sqlContext.read.format("com.databricks.spark.csv")\
         .option("header", "true")\
         .option("inferSchema", "true") \
         .option("nullValue", "-")\
         .load(filepath)
+
+    df = df.withColumn("DURATION", df.DURATION.cast('Double'))
+
+    return df
 
 
 def data_pre_proc(df,
@@ -37,6 +41,40 @@ def data_pre_proc(df,
     assembled_df = assembler.transform(df_r)
 
     return assembled_df
+
+
+def train_duration_model(training_df):
+    duration_lr = LinearRegression(maxIter=10, regParam=0.01, elasticNetParam=1.0).setLabelCol("DURATION").setFeaturesCol("features")
+
+    duration_lr_model = duration_lr.fit(training_df)
+
+    return duration_lr_model
+
+
+def getPredictionsLabels(model, test_data):
+    predictions = model.transform(test_data)
+
+    return predictions.rdd.map(lambda row: (row.prediction, row.DURATION))
+
+
+def save_train_info(model, predictions_and_labels, output, filepath = "hdfs://localhost:9000/btr/ctba/output.txt"):
+    with open(filepath, 'a') as outfile:
+        output += "Model:\n"
+        output += "Coefficients: %s\n" % str(model.coefficients)
+        output += "Intercept: %s\n" % str(model.intercept)
+        output += "Model info\n"
+
+        trainingSummary = RegressionMetrics(predictions_and_labels)
+
+        output += "RMSE: %f\n" % trainingSummary.rootMeanSquaredError
+        output += "MAE: %f\n" % trainingSummary.meanAbsoluteError
+        output += "r2: %f\n" % trainingSummary.r2
+
+        outfile.write(output)
+
+
+def save_model(model, filepath):
+    model.write().overwrite().save(filepath)
 
 
 if __name__ == "__main__":
@@ -60,29 +98,16 @@ if __name__ == "__main__":
 
     train, test = preproc_data.randomSplit([0.7, 0.3], 24)
 
-    #
-    # # Duration
-    # duration_model = train_duration_model(preproc_data)
-    #
-    # predictions_and_labels = getPredictionsLabels(duration_model, preproc_data)
-    # save_train_info(duration_model, predictions_and_labels, "Duration model\n", train_info_output_filepath)
-    #
-    # save_model(duration_model, duration_model_path_to_save)
-    #
-    # duration_model_loaded = LinearRegressionModel.load(duration_model_path_to_save)
-    #
-    # print duration_model_loaded.coefficients[0]
-    #
-    # # Crowdedness
-    # crowdedness_model = train_crowdedness_model(preproc_data)
-    #
-    # predictions_and_labels = getPredictionsLabels(duration_model, preproc_data)
-    # save_train_info(crowdedness_model, predictions_and_labels, "Crowdedness model\n", train_info_output_filepath)
-    #
-    # save_model(crowdedness_model, crowdedness_model_path_to_save)
-    #
-    # crowdedness_model_loaded = LinearRegressionModel.load(crowdedness_model_path_to_save)
-    #
-    # print crowdedness_model_loaded.coefficients[0]
+    # Duration
+    duration_model = train_duration_model(train)
+
+    predictions_and_labels = getPredictionsLabels(duration_model, test)
+    save_train_info(duration_model, predictions_and_labels, "Duration model\n", train_info_output_filepath)
+
+    save_model(duration_model, duration_model_path_to_save)
+
+    duration_model_loaded = LinearRegressionModel.load(duration_model_path_to_save)
+
+    print duration_model_loaded.coefficients[0]
 
     sc.stop()
